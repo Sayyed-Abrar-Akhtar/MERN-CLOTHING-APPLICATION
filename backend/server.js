52cbb005ca6b1cdf5b5f9596f337420fd8f0d77d
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-04-25 16:16:15 +0545

    Added carousel

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,6 @@ app.use(express.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('API is running..........');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/users', userRoutes);
@@ -47,6 +43,18 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running..........');
+  });
+}
 
 app.use(notFound);
 
